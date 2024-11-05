1424136c73560cb49266b772254ae3a494bc2664
mkpise <mkpise@wpi.edu>
2024-11-05 00:14:00 -0500

    Improved the home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,48 +1,27 @@
 "use client";
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const handleSellerSignIn = () => {
+    router.push('/seller/signin');
+  };
 
   return (
     <div>
-      <div className="navbar text-neutral-content">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">Auction 🏠</a>
-        </div>
-        <div className="navbar-end flex space-between">
-          <a className="m-2 ">Hey Customer!</a>
+      <div className="navbar text-neutral-content justify-center bg-black">
+        <a className="btn btn-ghost text-2xl font-bold">Fortnet Auction House 🏠</a>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+        <div className="flex items-center space-x-4">
+          <span className="text-lg">Hey Customer!</span>
           <a className="btn bg-primary">Sign in</a>
         </div>
+        <div>
+          <button onClick={handleSellerSignIn} className="btn bg-secondary mt-4">Seller? Click here</button>
+        </div>
       </div>
-
     </div>
   );
 }
