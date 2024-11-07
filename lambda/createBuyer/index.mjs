edaddf472916354fe610f138ba58093dbfbfcfce
Jai Patel <92940919+jaipatel2003@users.noreply.github.com>
2024-11-07 00:06:04 -0500

    Adding lambdas

---
+++ lambda/createBuyer/index.mjs
@@ -0,0 +1,38 @@
+import mysql from 'mysql'
+
+export const handler = async (event) => {
+  
+  
+  var pool = mysql.createPool({
+    host: "aucdb.cv0uky8u621r.us-east-2.rds.amazonaws.com",
+    user: "aucAdmin",
+    password: "aucPassword",
+    database: "AuctionHouse"
+  });
+  
+  
+  let insertBuyer = (email, password) => {
+    return new Promise((resolve, reject) => {
+      pool.query("INSERT INTO Buyers (username,password,isActive,funds) VALUES(?,?,1,0)", [email, password], (error, rows) => {
+        if (error) { 
+          return reject("Error: Username Already Exists") 
+        }
+        if ((rows) && (rows.affectedRows == 1)) {
+          return resolve("Buyer Added Successfully")
+        } 
+        else {
+          return reject("Error: Unable to add buyer")
+        }
+      });
+    });
+  }
+  
+  const email = await insertBuyer(event.email, event.password)
+  let result = email
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  }
+  
+};
